3dc6bee66af1a3fa23379991ddf1cbaf881ef969
italo <italovinicius2018@gmail.com>
2022-09-08 18:43:32 -0300

    🚧 wip: add style to Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+
+  width: 100%;
+  height: 116px;
+
+  display: flex;
+  justify-content: center;
+  padding: 42px 123px;
+
+`;
+
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,6 @@
 export default {
   COLORS: {
-    BACKGROUND_900: "#1C1B1E"
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_700: "#3E3B47"
   }
 }
