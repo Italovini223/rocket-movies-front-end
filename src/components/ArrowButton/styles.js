f8d7e6ca36a39682c3c8e84519983dc4746541a0
italo <italovinicius2018@gmail.com>
2022-09-10 20:34:55 -0300

    ✨ feat: add style to ArrowButton

---
+++ src/components/ArrowButton/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+
+  display: flex;
+  align-items: center;
+  
+  border: none;
+  background: none;
+
+  color: ${({theme}) => theme.COLORS.SALMON};
+`;
