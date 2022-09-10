1889efc87450da4bdf3e7c4e99a0aa9c70e231d9
italo <italovinicius2018@gmail.com>
2022-09-10 20:57:29 -0300

    ✨ feat: add inputs in Form

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,9 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+
 import { ArrowButton } from "../../components/ArrowButton";
+import { Input } from "../../components/Input";
+
+import {FiUser, FiMail, FiLock} from 'react-icons/fi'
 
 export function Profile() {
   return(
@@ -7,6 +11,29 @@ export function Profile() {
       <header>
         <ArrowButton title="Voltar" />
       </header>
+
+      <Form>
+        <Input
+          value="Ítalo Vinícius"
+          tipe="text"
+          icon={FiUser}
+        />
+            <Input
+          value="ÍtaloVinícius2018@gmail.com"
+          title="text"
+          icon={FiMail}
+        />
+        <Input
+          type="password"
+          placeholder="Senha Atual"
+          icon={FiLock}
+        />
+        <Input
+          type="password"
+          placeholder="Nova Senha"
+          icon={FiLock}
+        />
+      </Form>
     </Container>
   )
 }
