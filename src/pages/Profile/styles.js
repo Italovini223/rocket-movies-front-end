607df894f01715b879278d72c10367e3ff1c4831
italo <italovinicius2018@gmail.com>
2022-09-10 20:30:22 -0300

    ✨ feat: add a header to page Profile

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -3,4 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
+  > header {
+    width: 100%;
+    height: 144px;
+
+    background-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+  }
 `; 
