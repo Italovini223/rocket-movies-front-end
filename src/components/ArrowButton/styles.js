4527690e085a9563d5e72be8a7e5530c519a60c6
italo <italovinicius2018@gmail.com>
2022-09-10 20:39:28 -0300

    ✨ feat: add style to componentes in button

--- src/components/ArrowButton/styles.js
+++ src/components/ArrowButton/styles.js
@@ -9,4 +9,15 @@ export const Container = styled.button`
   background: none;
 
   color: ${({theme}) => theme.COLORS.SALMON};
+
+  > span {
+    font-size: 16px;
+    font-weight: 400;
+
+    color: ${({theme}) => theme.COLORS.SALMON};
+  }
+
+  > svg {
+    color: ${({theme}) => theme.COLORS.SALMON};
+  }
 `;
