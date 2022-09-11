67e70e3c38d9d576df24287972557d8f417631a9
italo <italovinicius2018@gmail.com>
2022-09-11 03:55:31 -0300

    ✨ feat: add style in form

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,40 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+
+import {Input} from '../../components/Input'
+import { Button} from '../../components/Button'
+import { ArrowButton} from '../../components/ArrowButton'
+
+import {FiUser, FiMail, FiLock} from 'react-icons/fi'
 
 export function SignIn(){
   return(
     <Container>
-      
+      <Form>
+        <h1>RocketMovies</h1>
+        <span>Aplicação para acompanhar tudo que assistir.</span>
+        <h2>Crie sua conta</h2>
+        <Input 
+          icon={FiUser}
+          placeholder="Nome"
+        />
+        <Input 
+          icon={FiUser}
+          type="text"
+          placeholder="Nome"
+        />
+        <Input 
+          icon={FiMail}
+          type="text"
+          placeholder="E-mail"
+        />
+        <Input 
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button title="Cadastrar"/>
+        <ArrowButton title="Voltar para o login"/>
+      </Form>
     </Container>
   )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,4 +3,49 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100;
   height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+
+  padding: 0 136px;
+
+  > h1 {
+    font-size: 48px;
+    font-weight: 700;
+
+    color: ${({theme}) => theme.COLORS.SALMON};
+  }
+
+  > span {
+    font-size: 14px;
+    font-weight: 400;
+
+    color: ${({theme}) => theme.COLORS.WHITE};
+
+    margin-bottom: 48px;
+
+  }
+
+  > h2 {
+    font-size: 24px;
+    font-weight: 500;
+
+    color: ${({theme}) => theme.COLORS.WHITE};
+
+    margin-bottom: 48px;
+  }
+
+  > button {
+    margin: 16px 0;
+  }
+
 `;
