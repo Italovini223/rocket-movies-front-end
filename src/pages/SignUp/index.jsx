2231eec3edfc3d296536e0ca5922c65c620286bc
italo <italovinicius2018@gmail.com>
2022-09-11 04:19:37 -0300

    ✨ feat: create SignUp page

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,41 @@
+import { Background, Container, Form } from "./styles";
+
+import {Input} from '../../components/Input'
+import { Button} from '../../components/Button'
+import { ArrowButton} from '../../components/ArrowButton'
+
+import {FiUser, FiMail, FiLock} from 'react-icons/fi'
+
+export function SignUp(){
+  return(
+    <Container>
+      <Background />
+      <Form>
+        <h1>RocketMovies</h1>
+        <span>Aplicação para acompanhar tudo que assistir.</span>
+        <h2>Crie sua conta</h2>
+        <Input 
+          icon={FiUser}
+          placeholder="Nome"
+        />
+        <Input 
+          icon={FiUser}
+          type="text"
+          placeholder="Nome"
+        />
+        <Input 
+          icon={FiMail}
+          type="text"
+          placeholder="E-mail"
+        />
+        <Input 
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button title="Cadastrar"/>
+        <ArrowButton title="Voltar para o login"/>
+      </Form>
+    </Container>
+  )
+}
