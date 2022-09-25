0f6cec503289e6d6952a1e09441de8ca8a848f73
italo <italovinicius2018@gmail.com>
2022-09-25 11:37:50 -0300

    ✨ feat: add line limit in description in noteItem

--- src/components/Note/styles.js
+++ src/components/Note/styles.js
@@ -39,6 +39,12 @@ export const Container = styled.button`
     color: ${({theme}) => theme.COLORS.GREY_400};
 
     text-align: justify;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2; /* number of lines to show */
+    -webkit-box-orient: vertical;
   }
 
   > footer {
--- src/styles/global.js
+++ src/styles/global.js
@@ -19,4 +19,5 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(0.9);
   }
+
 `;
