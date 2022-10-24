43ab6bc763685799905fd9c4e992714aa4ff919f
Italovini223 <italovinicius2018@gmail.com>
2022-10-24 00:18:31 -0300

    ♻️ refactor: change Note componente to button

--- src/components/Note/styles.js
+++ src/components/Note/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import {Link} from 'react-router-dom'
 
-export const Container = styled(Link)`
+export const Container = styled.button`
   width: 100%;
 
   text-decoration: none;
