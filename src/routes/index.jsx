f629c2952a498b75fcee5c2a03efd494b4fbe391
Italovini223 <italovinicius2018@gmail.com>
2022-10-28 20:26:31 -0300

    ♻️ refactor: change appRoutes file name

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter} from  'react-router-dom'
-import { AppRoutes } from './app.routes'
+import { AppRoutes } from './apps.routes'
 import { AuthRoutes } from './auth.routes'
 import { useAuth } from '../hooks/auth'
 export function Routes() {
