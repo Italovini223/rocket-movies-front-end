87b509cc5375c2a58a90763092c281dffa98291a
Italovini223 <italovinicius2018@gmail.com>
2022-10-28 20:32:19 -0300

    ♻️ refactor: change assets folder name

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import backgroundImg from "../../assets/background.png"
+import backgroundImg from "../../Assets/background.png"
  
 export const Container = styled.div`
   width: 100;
