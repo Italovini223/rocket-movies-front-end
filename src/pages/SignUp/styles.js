fd4d05d373a6ef62a8c9fb196bcbfdb3ba1d62ad
Italovini223 <italovinicius2018@gmail.com>
2022-10-28 20:49:14 -0300

    ♻️ refactor: change backgroudImg import

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import backgroundImg from '../../assets/background.png'
+import backgroundImg from '../../../public'
  
 export const Container = styled.div`
   width: 100;
