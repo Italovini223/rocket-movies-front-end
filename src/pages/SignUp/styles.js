4391509d4e772823e5ded9206b26567e667cd933
Italovini223 <italovinicius2018@gmail.com>
2022-10-28 20:50:26 -0300

    🐛 fix: fix backGround img import

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import backgroundImg from '../../../public'
+import backgroundImg from '../../../public/background.png'
  
 export const Container = styled.div`
   width: 100;
